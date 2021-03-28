2d0ec32ad6f24717b435d65ab9331f2d41b466b6
chenxiaobo <chenxiaobo@meizu.com>
2021-03-28 22:07:42 +0800

    feat: 增加环境变量，区别生产环境/开发环境，配置不同的数据库链接;

--- db.go
+++ db.go
@@ -16,6 +16,7 @@ var ctx context.Context
 var dbClient *qmgo.Client
 
 var MONGODB_URI string
+var ENV string
 
 func loadConfig() {
 	err := godotenv.Load()
@@ -24,6 +25,11 @@ func loadConfig() {
 	}
 
 	MONGODB_URI = os.Getenv("MONGODB_URI")
+	ENV = os.Getenv("ENV")
+	// 开发环境数据库链接
+	if ENV == "dev" {
+		MONGODB_URI = "mongodb://localhost:27017"
+	}
 }
 
 // 因为此函数依赖log，因此不能放在init函数中执行，需要后于log.go的init函数执行
